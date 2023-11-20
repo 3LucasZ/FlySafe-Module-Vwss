bf1f59dd4a897c81c7d1260fe5b0138bd6b1f62f
3LucasZ <lucas.zheng@warriorlife.net>
2023-11-19 19:16:09 -0800

    working wss server!!! AND AP !!! :)

--- src/wss_server_example.c
+++ src/wss_server_example.c
@@ -12,9 +12,10 @@
 #include "keep_alive.h"
 #include "sdkconfig.h"
 #include <esp_err.h>
+#include "esp_mac.h"
 
 #define EXAMPLE_ESP_WIFI_SSID       "FlySafeAP"
-#define EXAMPLE_ESP_WIFI_PASS       "FlySafe"
+#define EXAMPLE_ESP_WIFI_PASS       "FlySafePW"
 #define EXAMPLE_ESP_WIFI_CHANNEL    1
 #define EXAMPLE_MAX_STA_CONN        4
 
@@ -282,11 +283,23 @@ static void wss_server_send_messages(httpd_handle_t* server)
     }
 }
 //***
-
-void wifi_init_softap(void)
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
 {
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+esp_err_t wifi_init_softap(void)
+{
+    //ESP_ERROR_CHECK(esp_netif_init());
+    //ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -326,6 +339,7 @@ void wifi_init_softap(void)
 
     ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+    return ESP_OK;
 }
 
 //***
@@ -361,5 +375,7 @@ void app_main(void)
     /* This function demonstrates periodic sending Websocket messages
      * to all connected clients to this server
      */
+    //wss_server_send_messages(start_wss_echo_server());
+    server = start_wss_echo_server();
     wss_server_send_messages(&server);
 }
